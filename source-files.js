0fafaf6058b0397401f68f8affa31c3044ec7cca
Travis CI User <travis@example.org>
2019-05-05 20:03:16 +0000

    Documentation upload

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["serde_dynamodb"] = {"name":"","dirs":[{"name":"dynamodb","dirs":[],"files":["de.rs","mod.rs","ser.rs"]},{"name":"dynamodbstreams","dirs":[],"files":["de.rs","mod.rs","ser.rs"]}],"files":["error.rs","lib.rs"]};
+createSourceSidebar();
